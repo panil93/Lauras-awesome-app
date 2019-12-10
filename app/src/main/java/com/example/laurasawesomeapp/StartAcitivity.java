91c33c0cbb388ed911bfaf4baa3fb01620a30f86
panil93 <frulauraw@gmail.com>
2019-12-10 23:44:26 +0100

    Starting
    Opening of the project

---
+++ app/src/main/java/com/example/laurasawesomeapp/StartAcitivity.java
@@ -0,0 +1,17 @@
+package com.example.laurasawesomeapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StartAcitivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_acitivity);
+    }
+    public boolean checkIfInsigned(){
+        return false;
+    }
+}
