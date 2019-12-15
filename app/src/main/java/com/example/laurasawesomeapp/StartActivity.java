cf1258139d0ae68652c483035f62a319832a085d
panil93 <frulauraw@gmail.com>
2019-12-15 13:03:18 +0100

    Splash Activity with sigh in check

--- app/src/main/java/com/example/laurasawesomeapp/StartActivity.java
+++ app/src/main/java/com/example/laurasawesomeapp/StartActivity.java
@@ -6,13 +6,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
+import com.facebook.AccessToken;
+
 public class StartActivity extends AppCompatActivity {
     private static int SPLASH_TIME_OUT = 4000;
-    private boolean isTheUserAssigned = false;
+    private boolean isTheUserAssigned;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_acitivity);
+        AccessToken accessToken = AccessToken.getCurrentAccessToken();
+        isTheUserAssigned = accessToken != null && !accessToken.isExpired();
         new Handler().postDelayed(new Runnable(){
             @Override
             public void run(){
